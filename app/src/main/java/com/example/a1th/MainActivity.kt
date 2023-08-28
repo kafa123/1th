dbbce851375f9478c4960eb9cde60e24d1191d87
kafa123 <nurmuhammadkafabih2003@mail.ugm.ac.id>
2023-08-28 10:37:46 +0700

    selesai

--- app/src/main/java/com/example/a1th/MainActivity.kt
+++ app/src/main/java/com/example/a1th/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.a1th
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import com.example.a1th.databinding.ActivityMainBinding
 
@@ -13,9 +14,10 @@ class MainActivity : AppCompatActivity() {
 
         with(binding) {
             btnTest.setOnClickListener {
-                val enteredText = nameTextView.text.toString()
+                val enteredText ="welcome, "+ nameTextView.text.toString()
                 Toast.makeText(this@MainActivity, enteredText, Toast.LENGTH_SHORT).show()
             }
         }
     }
+fun forgottenPW(){}
 }
